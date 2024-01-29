1b7404aa260e114987e235952157f1acf0d79a04
DevRahat <dev.rahatislam@gmail.com>
2024-01-29 19:22:28 +0600

    join in Array

---
+++ join.js
@@ -0,0 +1,10 @@
+//join() in JavaScript Array
+const friends=["Rahul","Fahad","Tamal","Mortuza"];
+
+console.log(friends.join(' '));
+
+console.log(friends.join('+'));
+
+console.log(friends.join(','));
+
+console.log(friends.join('-'));
