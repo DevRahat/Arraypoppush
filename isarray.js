bb70f8253f750f8032d2f126f4d3dfe148067996
DevRahat <dev.rahatislam@gmail.com>
2024-01-29 19:01:32 +0600

    isArray

--- isarray.js
+++ isarray.js
@@ -1,6 +1,8 @@
 const friends=["Rahul","Fahad","Tamal","Mortuza"];
 
 const food= 'as lo ko ji';
+const num=[];
 
 console.log(Array.isArray(friends)); //we can check if any given list is an array or not. 
-console.log(Array.isArray(food));
+console.log(Array.isArray(food));   //output: false
+console.log(Array.isArray(num));    //output: true
