a037114592fde13b69bd3219cec910d936b44c90
DevRahat <dev.rahatislam@gmail.com>
2024-01-29 09:23:25 +0600

    includes method

---
+++ includes.js
@@ -0,0 +1,10 @@
+const friends=["Rahul","Fahad","Tamal","Mortuza"];
+console.log(friends.includes("Tamal"));
+
+console.log(friends.includes("Salehin"));
+
+if(friends.includes("Fahad")){
+    console.log("We will go for Dinner");
+}else{
+    console.log("We will stay at home");
+}
